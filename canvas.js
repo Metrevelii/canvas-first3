bd8065028cb83f82ceb31c84dcf78e20ae451def
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-06-30 11:47:13 +0400

    canvas resize

---
+++ canvas.js
@@ -0,0 +1,155 @@
+const canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const c = canvas.getContext("2d");
+
+// X and Y defines where this rectangle is going to be x=100 y=100;
+// c.fillStyle = "rgba(255, 0, 0, 0.1)";
+
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = "rgba(0, 0, 255, 0.1)";
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = "rgba(0, 255, 0, 0.1)";
+// c.fillRect(300, 300, 100, 100);
+
+// console.log(canvas);
+
+// // Drawing a Line
+
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3";
+// c.stroke();
+
+// Arc / Circle
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = "blue";
+// c.stroke();
+
+// for (let i = 0; i < 100; i++) {
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+//   let color = Math.floor(Math.random() * 16777215).toString(16);
+
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = "#" + color;
+//   c.stroke();
+// }
+
+// let x = Math.random() * innerWidth;
+// let y = Math.random() * innerHeight;
+// let dx = (Math.random() - 0.5) * 10;
+// let dy = (Math.random() - 0.5) * 10;
+// let radius = 30;
+let mouse = {
+  x: undefined,
+  y: undefined
+}
+
+let maxRadius = 40;
+let minRadius = 2;
+
+let colorArray = [
+  '#A305FA',
+  '#5504DE',
+  '#1F07F5',
+  '#0429DE',
+  '#056FFA',
+]
+
+window.addEventListener('mousemove', function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+})
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  };
+
+  this.update = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    // interactivity
+
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+      this.radius += 1;
+    } else if ( this.radius > this.minRadius ) {
+      this.radius -= 1;
+    }
+
+    this.draw();
+  };
+}
+
+// let x = Math.random() * innerWidth;
+// let y = Math.random() * innerHeight;
+// let dx = (Math.random() - 0.5) * 10;
+// let dy = (Math.random() - 0.5) * 10;
+// let radius = 30;
+
+
+let circleArray = [];
+
+function init() {
+  circleArray = [];
+
+  for (let i = 0; i < 1000; i++) {
+    let radius = Math.random() * 3 + 1;
+    let x = Math.random() * (innerWidth - radius * 2) + radius;
+    let y = Math.random() * (innerHeight - radius * 2) + radius;
+    let dx = Math.random() - 0.5;
+    let dy = Math.random() - 0.5;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
+}
+
+console.log(circleArray);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // clears old drawing, draws only one circle moving
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+init();
+animate();
